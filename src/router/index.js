2a4f5093653b8f489beb5d43a3a26320ff62d491
Spatchy <spatchy@spatchy.net>
2020-11-14 17:40:34 +0000

    Created login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import all views here
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Login',
+    component: Login
   },
   {
     path: '/about',
