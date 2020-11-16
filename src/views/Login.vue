14e5c60dca306262015edd7a427e9f8139967d33
Spatchy <spatchy@spatchy.net>
2020-11-16 20:51:08 +0000

    fixed to match what linter wants

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,33 +1,33 @@
 <template>
-    <div id="login">
-        <h1>Login</h1>
-        <input type="text" name="username" v-model="input.username" placeholder="Username" />
-        <input type="password" name="password" v-model="input.password" placeholder="Password" />
-        <button type="button" v-on:click="login()">Login</button>
-    </div>
+  <div id="login">
+    <h1>Login</h1>
+    <input type="text" name="username" v-model="input.username" placeholder="Username" />
+    <input type="password" name="password" v-model="input.password" placeholder="Password" />
+    <button type="button" v-on:click="login()">Login</button>
+  </div>
 </template>
 
 <script>
-    import io from 'socket.io'
-    export default {
-        name: 'Login',
-        data() {
-            return {
-                input: {
-                    username: "",
-                    password: ""
-                }
-            }
-        },
-        methods: {
-            login() {
-                var socket = io();
-                if(this.input.username != "" && this.input.password != "") {
-                    socket.emit("login-attempt", [this.username.input, this.password.input])
-                } else {
-                    console.log("A username and password must be present");
-                }
-            }
+  import io from 'socket.io'
+  export default {
+    name: 'Login',
+    data() {
+      return {
+        input: {
+          username: "",
+          password: ""
         }
+      }
+    },
+    methods: {
+      login() {
+        var socket = io();
+        if(this.input.username != "" && this.input.password != "") {
+          socket.emit("login-attempt", [this.username.input, this.password.input]);
+        } else {
+          console.log("A username and password must be present");
+        }
+      }
     }
+  }
 </script>
