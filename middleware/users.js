7f203f876d0317433038b32dc607802af6e9335a
James Batchelor <jlb75@kent.ac.uk>
2021-01-03 16:13:40 +0000

    removed unneeded indent

--- middleware/users.js
+++ middleware/users.js
@@ -1,56 +1,56 @@
 module.exports = {
-    validateRegister: (req, res, next) => {
-      // valid primary email
-      validEmailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g
-      
-      if (!validEmailRegex.test(req.body.username)) {
-        return res.status(400).send({
-          msg: 'Please enter a valid primary email address'
-        });
-      }
-
-      // why the hell does this need a new variable name?????
-      validUniEmailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g
-
-      if (!validUniEmailRegex.test(req.body.uniEmail) || !req.body.uniEmail.endsWith('.ac.uk')) {
-        return res.status(400).send({
-          msg: 'Please enter a valid university email address ending with .ac.uk'
-        });
-      }
-  
-      // password min 6 chars
-      if (!req.body.password || req.body.password.length < 6) {
-        return res.status(400).send({
-          msg: 'Please enter a password with min. 6 chars'
-        });
-      }
-  
-      // password (repeat) does not match
-      if (
-        !req.body.password_repeat ||
-        req.body.password != req.body.password_repeat
-      ) {
-        return res.status(400).send({
-          msg: 'Both passwords must match'
-        });
-      }
-  
-      next();
-    },
-    isLoggedIn: (req, res, next) => {
-      try {
-        const token = req.headers.authorization.split(' ')[1];
-        const decoded = jwt.verify(
-          token,
-          'SECRETKEY'
-        );
-        req.userData = decoded;
-        next();
-      } catch (err) {
-        return res.status(401).send({
-          msg: 'Your session is not valid!'
-        });
-      }
+  validateRegister: (req, res, next) => {
+    // valid primary email
+    validEmailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g
+    
+    if (!validEmailRegex.test(req.body.username)) {
+      return res.status(400).send({
+        msg: 'Please enter a valid primary email address'
+      });
     }
+
+    // why the hell does this need a new variable name?????
+    validUniEmailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g
+
+    if (!validUniEmailRegex.test(req.body.uniEmail) || !req.body.uniEmail.endsWith('.ac.uk')) {
+      return res.status(400).send({
+        msg: 'Please enter a valid university email address ending with .ac.uk'
+      });
+    }
+
+    // password min 6 chars
+    if (!req.body.password || req.body.password.length < 6) {
+      return res.status(400).send({
+        msg: 'Please enter a password with min. 6 chars'
+      });
+    }
+
+    // password (repeat) does not match
+    if (
+      !req.body.password_repeat ||
+      req.body.password != req.body.password_repeat
+    ) {
+      return res.status(400).send({
+        msg: 'Both passwords must match'
+      });
+    }
+
+    next();
+  },
+  isLoggedIn: (req, res, next) => {
+    try {
+      const token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(
+        token,
+        'SECRETKEY'
+      );
+      req.userData = decoded;
+      next();
+    } catch (err) {
+      return res.status(401).send({
+        msg: 'Your session is not valid!'
+      });
+    }
+  }
 
 };
