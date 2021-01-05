cef180acfe065ef54a9366e59e305625814c0612
justinas pocevicius <jp655@kent.ac.uk>
2021-01-05 19:04:24 +0000

    Restrictions on DOB, input check on SignUp

--- middleware/users.js
+++ middleware/users.js
@@ -1,5 +1,8 @@
 module.exports = {
   validateRegister: (req, res, next) => {
+
+    var errorMessages = [];
+
     // valid primary email
     validEmailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g
     
@@ -32,6 +35,16 @@ module.exports = {
     ) {
       return res.status(400).send({
         msg: 'Both passwords must match'
+      });
+    }
+    
+    // checking if birth date is valid, person has >= than 16 years
+    var currentDate = new Date(); 
+    var currentYear = currentDate.getFullYear();
+    var usersBirthYear = req.body.dob.substring(0, 4);
+    if((currentYear - usersBirthYear) <= 16 || (usersBirthYear > currentYear)) {
+      return res.status(400).send({
+        msg: 'Please enter valid birth date.'
       });
     }
 
@@ -80,5 +93,4 @@ module.exports = {
       });
     }
   }
-
 };
