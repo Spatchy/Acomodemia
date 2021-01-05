4cb1fab5ab0c6b16d6ba56d5ebc9df9cd14bd3ac
justinas pocevicius <jp655@kent.ac.uk>
2021-01-05 19:03:33 +0000

    Added dropDown menu for gender. Some styling

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,17 +1,45 @@
 <template>
-  <div>
+  <div id="signUp">
     <h1>Sign Up</h1>
-    <input type="text" placeholder="Username" v-model="username" />
+    <label>Personal Email Address:</label>
+    <input type="text" placeholder="Your Personal Email" v-model="username" />
+    <br>
+    <label>First Name:</label>
     <input type="text" placeholder="First Name" v-model="firstName" />
+    <br>
+    <label>Second Name:</label>
     <input type="text" placeholder="Second Name" v-model="secondName" />
+    <br>
+    <label>Date Of Birth:</label>
     <input type="date" placeholder="Date of birth" v-model="dob" />
+    <br>
+    <label>University Email:</label>
     <input type="text" placeholder="University email" v-model="uniEmail" />
-    <input type="text" placeholder="Gender" v-model="gender" />
+    <!-- <input type="text" placeholder="Gender" v-model="gender" /> -->
 
+    <br>
+
+    <label>Gender:</label>
+      <select v-model="gender">
+        <option v-for="gend in genders" :key="gend">{{gend}}</option>
+      </select>
+
+    <br>
+
+    <label>Password:</label>
     <input type="password" placeholder="Password" v-model="password" />
+    <br>
+    <label>Repeat Password:</label>
     <input type="password" placeholder="Password (repeat)" v-model="password_repeat" />
+    <br>
     <input type="button" @click="signUp" value="Sign Up" />
+    <br>
     <p v-if="msg">{{ msg }}</p>
+    <!-- <p v-if="msg.length !== 0">
+      <ul>
+        <li v-for="error in errors" v-bind:key="error"> {{error}}</li>
+      </ul>
+    </p>   -->
   </div>
 
 </template>
@@ -25,6 +53,7 @@ export default {
   },
   data () {
     return {
+      genders: ['Female', 'Male', 'Secret'],
       username: '',
       firstName: '',
       secondName: '',
@@ -58,7 +87,7 @@ export default {
         const user = response.user
         this.$store.dispatch('login', { token, user })
 
-        this.$router.push('/verify');
+        this.$router.push('/verify')
       } catch (error) {
         this.msg = error.response.data.msg
       }
@@ -66,3 +95,16 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  label {
+    width:180px;
+    clear:left;
+    text-align:right;
+    padding-right:10px;
+}
+
+input, label {
+    float:left;
+}
+</style>
