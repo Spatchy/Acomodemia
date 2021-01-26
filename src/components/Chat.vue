d7c26c71cc7851e7a9fb3a9201cf57d48627380f
harry <harrybittan@gmail.com>
2021-01-26 12:15:05 +0000

    added chat component

---
+++ src/components/Chat.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <input type="text" placeholder="Type your message here" v-model="message"/>
+        <input type="button" value="Send" @click="send" />
+    </div>
+</template>
+<script>
+import AuthService from '@/services/AuthService.js'
+
+export default {
+    name: 'Chat',
+    props: ['name, picture, matchingID'],
+    data() {
+        return {
+            message: '',
+            recipient: ''
+        }
+    },
+    methods: {
+        async send() {
+            //send the message to server
+        }
+    }
+}
+</script>
