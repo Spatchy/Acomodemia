85a1da5aea9f5ea9c22c598ef57daeecea57a5fb
karunsehmi <kss26@kent.ac.uk>
2021-01-27 21:46:16 +0000

    Most of Signup page styling done

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,45 +1,157 @@
 <template>
-  <div id="signUp">
-    <h1>Sign Up</h1>
-    <label>Personal Email Address:</label>
-    <input type="text" placeholder="Your Personal Email" v-model="username" />
-    <br>
-    <label>First Name:</label>
-    <input type="text" placeholder="First Name" v-model="firstName" />
-    <br>
-    <label>Second Name:</label>
-    <input type="text" placeholder="Second Name" v-model="secondName" />
-    <br>
-    <label>Date Of Birth:</label>
-    <input type="date" placeholder="Date of birth" v-model="dob" />
-    <br>
-    <label>University Email:</label>
-    <input type="text" placeholder="University email" v-model="uniEmail" />
-    <!-- <input type="text" placeholder="Gender" v-model="gender" /> -->
+  <div> 
+    <div class="container">
+      <div>
+        <h1 class="title is-1">Sign Up</h1>
 
-    <br>
-    <label>Gender:</label>
-      <select v-model="gender">
-        <option v-for="gend in genders" :key="gend">{{gend}}</option>
-      </select>
-    <br>
 
-    <label>Password:</label>
-    <input type="password" placeholder="Password" v-model="password" />
-    <br>
-    <label>Repeat Password:</label>
-    <input type="password" placeholder="Password (repeat)" v-model="password_repeat" />
-    <br>
-    <input type="button" @click="signUp" value="Sign Up" />
-    <br>
-    <p v-if="msg">{{ msg }}</p>
-    <!-- <p v-if="msg.length !== 0">
-      <ul>
-        <li v-for="error in errors" v-bind:key="error"> {{error}}</li>
-      </ul>
-    </p>   -->
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">First Name:</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <p class="control is-expanded">
+            <input class="input is-rounded is-info" type="text" placeholder="First Name" v-model="firstName" /> 
+            </p>
+          </div>
+
+        <div class="field-label is-normal">
+          <label class="label">Second Name:</label>
+        </div>
+          <div class="field">
+            <p class="control is-expanded">
+                <input class="input is-rounded is-info" type="text" placeholder="Second Name" v-model="secondName" />
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">University Email:</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <p class="control is-expanded">
+              <input class="input is-rounded is-info" type="text" placeholder="e.g. bs234@kent.ac.uk" v-model="uniEmail" />
+            </p>
+          </div>
+
+        <div class="field-label is-normal">
+          <label class="label">Personal Email:</label>
+        </div>
+          <div class="field">
+            <p class="control is-expanded">
+              <input class="input is-rounded is-info" type="text" placeholder="e.g. bobsmith@gmail.com" v-model="username" />
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">Date of Birth:</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <p class="control is-expanded">
+            <input class="input is-rounded is-info" type="date" placeholder="Date of birth" v-model="dob" />
+            </p>
+          </div>
+
+        <div class="field-label is-normal">
+          <label class="label">Gender:</label>
+        </div>
+          <div class="field">
+            <p class="control is-expanded">
+              <div class="select is-rounded is-info">
+                <select v-model="gender">
+                  <option v-for="gend in genders" :key="gend">
+                    {{gend}}
+                  </option>
+                </select>
+              </div>
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">Password:</label>
+        </div>
+        <div class="field-body">
+          <div class="field">
+            <p class="control is-expanded">
+              <input class="input is-rounded is-info" type="password" placeholder="Password" v-model="password" />
+            </p>
+          </div>
+
+        <div class="field-label is-normal">
+          <label class="label">Confirm Password:</label>
+        </div>
+          <div class="field">
+            <p class="control is-expanded">
+              <input class="input is-rounded is-info" type="password" placeholder="Password (repeat)" v-model="password_repeat" />
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+        <!-- 
+        <label class="label left">Personal Email Address:</label>
+        <input class="input is-rounded" type="text" placeholder="Your Personal Email" v-model="username" />
+        <br>
+
+        <label class="label left">First Name:</label>
+        <input class="input is-rounded" type="text" placeholder="First Name" v-model="firstName" />
+        <br>
+        
+        <label class="label left">Second Name:</label>
+        <input class="input is-rounded" type="text" placeholder="Second Name" v-model="secondName" />
+        <br>
+        
+        <label class="label left">Date Of Birth:</label>
+        <input class="input is-rounded" type="date" placeholder="Date of birth" v-model="dob" />
+        <br>
+        
+        <label class="label left">University Email:</label>
+        <input class="input is-rounded" type="text" placeholder="University email" v-model="uniEmail" />
+        <br>
+        
+        <label class="label left">Gender:</label>
+        <div class="select is-rounded signup">
+          <select v-model="gender">
+            <option v-for="gend in genders" :key="gend">{{gend}}</option>
+          </select>
+        </div>
+        <br>
+
+        <label class="label left">Password:</label>
+        <input class="input is-rounded" type="password" placeholder="Password" v-model="password" />
+        <br>
+        
+        <label class="label left">Repeat Password:</label>
+        <input class="input is-rounded" type="password" placeholder="Password (repeat)" v-model="password_repeat" />
+        <br>
+        -->
+       
+       
+        <input class="button is-rounded is-info" type="button" @click="signUp" value="Sign Up" />
+        <br>
+        <p v-if="msg">{{ msg }}</p>
+        <p v-if="msg.length !== 0">
+          <ul>
+            <li v-for="error in errors" v-bind:key="error"> {{error}}</li>
+          </ul>
+        </p> 
+    </div>
   </div>
-
 </template>
 
 <script>
