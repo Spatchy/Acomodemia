a916e227d13c99503516284163bc8e61f72df5b9
James Batchelor <jlb75@kent.ac.uk>
2021-01-29 20:19:56 +0000

    added disconnect handling

--- routes/chat.js
+++ routes/chat.js
@@ -7,15 +7,12 @@ module.exports = {
     var clients = {} // maps MatchingID of connected clients to their socket ID
 
     io.use(function(socket, next) {
-      console.log(socket.handshake.query)
-      console.log(socket.handshake.query.token)
         if (socket.handshake.query && socket.handshake.query.token) {
-          jwt.verify(socket.handshake.query.token, 'SECRETKEY', function(err, decoded) {
+          jwt.verify(socket.handshake.query.token, 'SECRETKEY', function (err, decoded) {
             if (err) {
               console.log(err)
               return next(new Error('Authentication error'))
             }
-            
             socket.decoded = decoded
             next()
           });
@@ -32,11 +29,19 @@ module.exports = {
               console.log(err)
             }
             else{
+              // add their matching id and this socket id to clients
               clients[result[0].MatchingID] = socket.id
               console.log(clients)
+
+              // remove them on disconnect
+              socket.on('disconnect', () => {
+                delete clients[result[0].MatchingID]
+                console.log(clients)
+              })
             }
           }
         )
+        
     })
   }
 }
