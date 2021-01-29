2ea4cd598927e90fc929540d0ddba6a1cf980829
James Batchelor <jlb75@kent.ac.uk>
2021-01-29 20:02:07 +0000

    Removed chat component from MatchedPerson

--- src/components/MatchedPerson.vue
+++ src/components/MatchedPerson.vue
@@ -2,18 +2,13 @@
     <div>
         <h3 id="name">{{name}} </h3><h3 id="age"> {{age}} </h3>
         <profile-pic>
-        <chat :name="name" :matchingID="matchingID">
     </div>
 </template>
 
 <script>
-import chat from '@/components/Chat.vue'
 export default {
   name: 'MatchedPerson',
   props: ['name', 'age', 'matchingID'],
-  components: {
-    chat
-  },
   async created() {
     console.log(this.matchingID)
   }
