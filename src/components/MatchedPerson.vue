8f80c5c709534f3bf629028f8b87a772cc750618
James Batchelor <jlb75@kent.ac.uk>
2021-01-30 15:56:24 +0000

    implemented click event to redirect to chat

--- src/components/MatchedPerson.vue
+++ src/components/MatchedPerson.vue
@@ -1,16 +1,22 @@
 <template>
-    <div>
+    <div @click="clickEvent">
         <h3 id="name">{{name}} </h3><h3 id="age"> {{age}} </h3>
         <profile-pic>
     </div>
 </template>
 
 <script>
+import router from '../router'
 export default {
   name: 'MatchedPerson',
   props: ['name', 'age', 'matchingID'],
   async created() {
     console.log(this.matchingID)
+  },
+  methods: {
+    clickEvent: function () {
+      router.push('/chat?to=' + this.matchingID)
+    }
   }
 }
 </script>
