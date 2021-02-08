a5147cc86e77e54b1873d149ec7824ce703143b8
James Batchelor <jlb75@kent.ac.uk>
2021-02-08 20:31:11 +0000

    removed prettier, reinitialised eslint to Google

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,24 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true,
   },
-  extends: [
+  'extends': [
     'plugin:vue/essential',
-    "plugin:prettier/recommended",
-    '@vue/standard'
+    'google',
   ],
-  parserOptions: {
-    parser: 'babel-eslint'
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
   },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'vue',
+  ],
+  'rules': {
+  },
+};
