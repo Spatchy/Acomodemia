785cfa1d745c1fd91461978695ab18766c9c1253
James Batchelor <jlb75@kent.ac.uk>
2021-02-09 23:47:14 +0000

    added interest tag component for use in feeditem

---
+++ src/components/InterestTag.vue
@@ -0,0 +1,17 @@
+<template>
+  <span class="tag is-rounded">
+    {{interest}}
+  </span>
+</template>
+<script>
+export default {
+  name: 'InterestTag',
+  props: ['interest'],
+};
+</script>
+<style scoped>
+.tag{
+  margin-right: 2px;
+  margin-left: 0px;
+}
+</style>
