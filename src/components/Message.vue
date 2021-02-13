1d502b27152d7de457959b30bbdbe49ecc594cdf
James Batchelor <jlb75@kent.ac.uk>
2021-02-13 23:49:55 +0000

    msg now aligns right/left based on sent/recieved

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,8 +1,21 @@
 <template>
-    <div v-bind:class="{Sent: sent, recieved : !sent }" :messageId="messageID">
-       <p> <span> {{message}} </span> </p>
+    <div >
+      <div :class="{'level': true, 'sent': sent, 'received': !sent}">
+        <div :class="{'level-right': sent,'level-left': !sent}">
+          <span :class="{
+            'level-item': true,
+            'sent': sent,
+            'received': !sent,
+            'has-background-primary': sent,
+            'has-text-white': sent,
+            'has-background-white': !sent,
+            'has-text-primary': !sent
+          }" :messageId="messageID">
+            {{message}}
+          </span>
+        </div>
+      </div>
     </div>
-
 </template>
 <script>
 export default {
@@ -10,18 +23,28 @@ export default {
   props: ['message', 'messageID', 'sent'],
 };
 </script>
-<style>
-.Sent{
-    background-color: #3498db;
-    color: white;
-    float: right;
+<style scoped>
+.level{
+  max-width:85%;
 }
-.recieved {
-    background-color: #EFF0EB;
-    color: #3498db;
-    float: left;
+.level-item{
+  text-align: left;
+  overflow-wrap: break-word;
 }
-p span {
-  display: block;
+.sent, .level-right{
+  margin-left: auto;
+  margin-right: 0;
+}
+.received, .level-left{
+  margin-right: auto;
+  margin-left: 0;
+}
+.level.sent{
+  margin-left: auto;
+  margin-right: 0.25rem;
+}
+.level.received{
+  margin-right: auto;
+  margin-left: 0.25rem;
 }
 </style>
