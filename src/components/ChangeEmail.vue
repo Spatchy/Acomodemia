98fa26fa88cad43d49f5ce4ddb9c35f372b71491
Ethan <ecko2@kent.ac.uk>
2021-02-15 15:44:42 +0000

    settings styling update

--- src/components/ChangeEmail.vue
+++ src/components/ChangeEmail.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
-        <div class="container1">
-          <h2>Please enter and confirm the email address you wish to change to, and your password</h2>
-          <input class="input is-rounded is-info" type="text" placeholder="e.g. bobsmith@gmail.com" v-model="newEmail" />
-          <input class="input is-rounded is-info" type="text" placeholder="Confirm your new email" v-model="newEmailConf" />
-          <input class="input is-rounded is-info" type="password" placeholder="*******" v-model="password"  />
-          <input class="button is-rounded is-info" type="button" @click="submit" value="Change Email" />
+        <div class="columns">
+          <div class="column is-three-fifths is-offset-one-fifth">
+            <h2>Please enter and confirm the email address you wish to change to, and your password</h2>
+            <input class="input is-rounded is-info" type="text" placeholder="e.g. bobsmith@gmail.com" v-model="newEmail" />
+            <input class="input is-rounded is-info" type="text" placeholder="Confirm your new email" v-model="newEmailConf" />
+            <input class="input is-rounded is-info" type="password" placeholder="*******" v-model="password"  />
+            <input class="button is-rounded is-info" type="button" @click="submit" value="Change Email" />
+          </div>
         </div>
     </div>
 </template>
