e9c8f8defb40c55daad8bb07643f2050bd7bbd1a
harry <harrybittan@gmail.com>
2021-02-23 12:57:23 +0000

    Made matches list a separate component

---
+++ src/components/Matches.vue
@@ -0,0 +1,48 @@
+<template>
+    <div ref="matchedlist">
+        <div id="no-matches-info" v-if="matchesList.length==0">
+            <div class="icon">
+              <i class="fas fa-users title is-1"></i>
+            </div>
+            <p class="subtitle is-6 left-align">You don't have any matches yet, use the feed to find some</p>
+          </div>
+    </div>
+</template>
+<script>
+import AuthService from '@/services/AuthService.js';
+import MatchedPerson from '@/components/MatchedPerson.vue';
+import Vue from 'vue';
+export default {
+  name: 'Matches',
+  components: {
+    MatchedPerson,
+  },
+  data() {
+    return {
+      matchesList: [],
+    };
+  },
+  methods: {
+    displayMatches(match) {
+      const ComponentClass = Vue.extend(MatchedPerson);
+      const instance = new ComponentClass({
+        propsData: {
+          name: match.name,
+          age: match.age,
+          matchingID: match.matchingID,
+          photo: match.photo,
+        },
+      });
+      instance.$mount(); // pass nothing
+      this.$refs.matchedlist.appendChild(instance.$el);
+    },
+  },
+  async created() {
+    this.matchesList = await AuthService.getMatches();
+    this.matchesList.forEach((element) => {
+      this.displayMatches(element);
+      console.log(element.matchingID);
+    });
+  },
+};
+</script>
--- src/views/Matching.vue
+++ src/views/Matching.vue
@@ -1,16 +1,11 @@
 <template>
     <div>
       <div class="columns">
-        <div class="column is-one-quarter" ref="matchedlist" id="matchedList">
+        <div class="column is-one-quarter" id="matchedList">
           <div class="box">
             <h2 class="title is-2">Your Matches</h2>
           </div>
-          <div id="no-matches-info" v-if="matchesList.length==0">
-            <div class="icon">
-              <i class="fas fa-users title is-1"></i>
-            </div>
-            <p class="subtitle is-6 left-align">You don't have any matches yet, use the feed to find some</p>
-          </div>
+          <matches>
         </div> <!--matches will be injected-->
         <div class="column">
             <div class="box">
@@ -23,36 +18,14 @@
 </template>
 <script>
 import Feed from '@/components/Feed.vue';
-import AuthService from '@/services/AuthService.js';
-import MatchedPerson from '@/components/MatchedPerson.vue';
-import Vue from 'vue';
+import Matches from '@/components/Matches.vue';
 
 export default {
   name: 'Matching',
-  data() {
-    return {
-      matchesList: [],
-    };
-  },
   components: {
     Feed,
     // eslint-disable-next-line vue/no-unused-components
-    MatchedPerson,
-  },
-  methods: {
-    displayMatches(match) {
-      const ComponentClass = Vue.extend(MatchedPerson);
-      const instance = new ComponentClass({
-        propsData: {
-          name: match.name,
-          age: match.age,
-          matchingID: match.matchingID,
-          photo: match.photo,
-        },
-      });
-      instance.$mount(); // pass nothing
-      this.$refs.matchedlist.appendChild(instance.$el);
-    },
+    Matches,
   },
   async mounted() {
     if (!this.$store.getters.isLoggedIn) {
@@ -64,13 +37,6 @@ export default {
     if (!this.$store.getters.getUser.EssentialSettingsComplete) {
       this.$router.push('/Complete'); // redirect user to complete their essential settings
     }
-  },
-  async created() {
-    this.matchesList = await AuthService.getMatches();
-    this.matchesList.forEach((element) => {
-      this.displayMatches(element);
-      console.log(element.matchingID);
-    });
   },
 };
 </script>
