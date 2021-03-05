43d076f2cd417423037caa54372dc18e84d0662f
James Batchelor <jlb75@kent.ac.uk>
2021-03-05 02:19:30 +0000

    partailly implemented cropper

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -27,10 +27,30 @@
         </div>
       </div>
     </form>
+
+    <!--Image cropping modal-->
+    <div :class="['modal', modalClass]">
+      <div class="modal-background" @click="cancelUpload()"></div>
+      <div class="modal-content">
+        <div>
+          <img :src="formattedPhoto" alt="Crop your photo" id="cropImage" ref="cropImage">
+        </div>
+        <div class="box">
+          <div class="level">
+            <div class="level-item">
+              <button class="button is-primary">Test</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button class="modal-close is-large" aria-label="close" @click="cancelUpload()"></button>
+    </div>
   </div>
 </template>
 <script>
 import AuthService from '@/services/AuthService.js';
+import Cropper from 'cropperjs';
+import 'cropperjs/dist/cropper.css';
 export default {
   name: 'FileUpload',
   data() {
@@ -38,6 +58,8 @@ export default {
       file: '',
       fileName: 'No file chosen…',
       message: '',
+      modalClass: '',
+      formattedPhoto: '',
     };
   },
   methods: {
@@ -49,9 +71,30 @@ export default {
       console.log(file);
       if (!allowedTypes.includes(file.type)) {
         this.message = 'Only images are allowed!';
-      }
-      if (file.size > 1500000) {
+      } else if (file.size > 1500000) {
         this.message = 'Your file is too large! max size is 1.5MB';
+      } else {
+        this.message = ''; // reset message
+
+        // image cropping stuff
+        this.modalClass = 'is-active';
+        try {
+          const reader = new FileReader();
+          reader.readAsDataURL(this.file);
+          const refThis = this; // keep a reference to this for the onload function
+          reader.onload = function() {
+            refThis.formattedPhoto = reader.result;
+            setTimeout(function() {
+              const cropper = new Cropper(refThis.$refs.cropImage, {
+                aspectRatio: 1 / 1,
+                viewMode: 3,
+              });
+              console.log(cropper);
+            }, 10);
+          };
+        } catch (err) {
+          console.error(err);
+        }
       }
     },
     async onSubmit() {
@@ -65,6 +108,11 @@ export default {
         this.message = 'Something went wrong';
       }
     },
+    cancelUpload() {
+      this.modalClass = '';
+      this.file = '';
+      this.fileName = 'No file chosen…';
+    },
   },
 };
 </script>
@@ -72,8 +120,18 @@ export default {
 span.button{
   width: auto;
 }
-
 input.file-name{
   border-width: 2px;
+}
+img, .container, .cropper-container{
+  height: 100%;
+}
+img{
+  object-fit: contain;
+  max-width: 100%;
+}
+.box{
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
 }
 </style>
