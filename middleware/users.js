b4fd07bb62fb7ad3015e4bf41f7cb4037f877cb0
karunsehmi <kss26@kent.ac.uk>
2021-03-12 16:17:28 +0000

    Issue #20 Fixed

--- middleware/users.js
+++ middleware/users.js
@@ -1,6 +1,21 @@
 const jwt = require('jsonwebtoken');
 module.exports = {
   validateRegister: (req, res, next) => {
+
+    // valid firstname
+    if (req.body.firstName.length <= null) {
+      return res.status(400).send({
+        msg: 'Please enter your First Name',
+      });
+    }
+
+    // valid surname
+    if (req.body.secondName.length <= null) {
+      return res.status(400).send({
+        msg: 'Please enter your Second Name',
+      });
+    }
+
     // valid primary email
     validEmailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
 
