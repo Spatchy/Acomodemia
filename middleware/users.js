141cbfd67a929c78618d601aca6eb0d253a8403f
karunsehmi <kss26@kent.ac.uk>
2021-03-13 15:02:33 +0000

    Issue #20 Completed

--- middleware/users.js
+++ middleware/users.js
@@ -3,16 +3,67 @@ module.exports = {
   validateRegister: (req, res, next) => {
 
     // valid firstname
-    if (req.body.firstName.length <= null) {
+    validfirstName = /^[^\s].+[^\s]$/g;
+
+    if (req.body.firstName.length <= 0 || null || undefined) {
       return res.status(400).send({
         msg: 'Please enter your First Name',
       });
     }
+    if (req.body.secondName.length > 32) {
+      return res.status(400).send({
+        msg: 'First Name character limit is 32. Please Try Again',
+      });
+    }
+    if (!validfirstName.test(req.body.firstName)) {
+      return res.status(400).send({
+        msg: 'Please enter a valid First Name',
+      });
+    }
 
     // valid surname
-    if (req.body.secondName.length <= null) {
+    validsecondName = /^[^\s].+[^\s]$/g;
+
+    if (req.body.secondName.length <= 0 || null || undefined) {
       return res.status(400).send({
         msg: 'Please enter your Second Name',
+      });
+    }
+    if (req.body.secondName.length > 32) {
+      return res.status(400).send({
+        msg: 'Second Name character limit is 32. Please Try Again',
+      });
+    }
+    if (!validsecondName.test(req.body.secondName)) {
+      return res.status(400).send({
+        msg: 'Please enter a valid Second Name',
+      });
+    }
+
+    // valid gender
+    if (req.body.gender.length <= 0 || null || undefined) {
+      return res.status(400).send({
+        msg: 'Please select your Gender',
+      });
+    }
+
+    // valid date of birth
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const usersBirthYear = req.body.dob.substring(0, 4);
+    if ((currentYear - usersBirthYear) <= 17) {
+      return res.status(400).send({
+        msg: 'To register you need to be older than 17 years.',
+      });
+    }
+    if ((usersBirthYear >= currentYear)) {
+      return res.status(400).send({
+        msg: 'You have entered future date. Are you time traveler? If not - please double check DOB input. Otherwise - contact MIB agency',
+      });
+    }
+    if (req.body.dob.length <= 0 || null || undefined) {
+      return res.status(400).send({
+        msg: 'Please enter your Date of Birth',
       });
     }
 
@@ -25,7 +76,7 @@ module.exports = {
       });
     }
 
-    // why the hell does this need a new variable name?????
+    // valid uni email
     validUniEmailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
 
     if (!validUniEmailRegex.test(req.body.uniEmail) || !req.body.uniEmail.endsWith('.ac.uk')) {
@@ -51,24 +102,9 @@ module.exports = {
       });
     }
 
-    // checking if birth date is valid, person has >= than 16 years
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-    const usersBirthYear = req.body.dob.substring(0, 4);
-    if ((currentYear - usersBirthYear) <= 17) {
-      return res.status(400).send({
-        msg: 'To register you need to be older than 17 years.',
-      });
-    }
-    if ((usersBirthYear >= currentYear)) {
-      return res.status(400).send({
-        msg: 'You have entered future date. Are you time traveler? If not - please double check DOB input. Otherwise - contact MIB agency',
-      });
-    }
-
-
     next();
   },
+
   isLoggedIn: (req, res, next) => {
     try {
       const token = req.headers.authorization.split(' ')[1];
@@ -84,6 +120,7 @@ module.exports = {
       });
     }
   },
+
   isVerified: (req, res, next) => {
     const db = require('../db.js');
     try {
